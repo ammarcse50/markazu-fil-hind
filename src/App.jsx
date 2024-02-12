794968bdde217d895f5250dbf783764b3193dd6d
ammmar <ammaraslam7164@gmail.com>
2024-02-12 13:18:48 +0600

    1st

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h2 className='text-3xl bg-green-300'>ppp</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer/Root/Root.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+
+            <Outlet></Outlet>
+            
+        </div>
+    );
+};
+
+export default Root;
