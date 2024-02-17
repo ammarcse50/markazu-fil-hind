0e2b7d98ab4d028e825a34c3412cee4b0ffb6bcf
ammmar <ammaraslam7164@gmail.com>
2024-02-17 22:59:37 +0600

    minimal-done-without-book

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,12 +1,17 @@
-
 import { useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="flex items-center  justify-center"> 
-      <h1 className="text-4xl">Oops!!!</h1> 
-      <button className="btn btn-primary  btn-outline btn-secondary" onClick={() => navigate(-1)}>Go Back</button>
+    <div className="flex items-center  justify-center">
+      <h1 className="text-4xl">Error!! Oops!!!</h1>
+      <br />
+      <button
+        className="btn btn-primary  btn-outline btn-secondary"
+        onClick={() => navigate(-1)}
+      >
+        Go Back
+      </button>
     </div>
   );
 };
